f17323b068127ede3fbfff9464a3796daec9e907
gryprilliams <gageryanprince@gmail.com>
2022-12-11 06:20:49 -0500

    Initial commit

---
+++ model/model.js
@@ -0,0 +1,185 @@
+var homeContent = `<div class="home">
+<section class="hero yellow-tint">
+    <h1>Header goes here</h1>
+    <h2>Less important text goes here</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+    <p>Fugiat aliquid minus nemo sed est.</p>
+    <input type="submit" value="Read More">
+</section>
+<section class="quote">
+    <p>“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo”
+    </p>
+    <h1>John Smith</h1>
+    <h2>Corporation CEO, books author</h2>
+</section>
+<section class="events">
+    <div class="title">
+        <h1>UPCOMING EVENTS:</h1>
+    </div>
+    <div class="eventcontainer">
+        <div class="datecontainer">
+            <div class="date">
+                <h1>06</h1>
+                <h2>JUN</h2>
+            </div>
+            <div class="text">
+                <h1>Sed et persipiatis.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="datecontainer">
+            <div class="date">
+                <h1>30</h1>
+                <h2>JUL</h2>
+            </div>
+            <div class="text">
+                <h1>Sed et persipiatis.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="datecontainer">
+            <div class="date">
+                <h1>30</h1>
+                <h2>AUG</h2>
+            </div>
+            <div class="text">
+                <h1>Sed et persipiatis.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="datecontainer">
+            <div class="date">
+                <h1>23</h1>
+                <h2>NOV</h2>
+            </div>
+            <div class="text">
+                <h1>Sed et persipiatis.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+        <div class="datecontainer">
+            <div class="date">
+                <h1>23</h1>
+                <h2>DEC</h2>
+            </div>
+            <div class="text">
+                <h1>Sed et persipiatis.</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
+            </div>
+        </div>
+    </div>
+</section>
+</div>`;
+var aboutContent = `<div class="about">
+<section class="abouthero yellow-tint"></section>
+<section class="history">
+    <h1>OUR HISTORY:</h1>
+    <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem.</p>
+</section>
+</div>`;
+var galleryContent = `<div class="gallery">
+<h1>GALLERY:</h1>
+<div class="row1">
+    <div class="food yellow-tint"></div>
+    <div class="dj pink-tint"></div>
+    <div class="speech green-tint"></div>
+</div>
+<div class="row2">
+    <div class="openfood purple-tint"></div>
+    <div class="international turquoise-tint"></div>
+</div>
+</div>`;
+var blogContent = `<div class="blog">
+<h3>BLOG:</h3>
+<div class="container">
+    <div class="blogcontainer">
+        <div class="date">
+            <h1>06</h1>
+            <h2>JUN</h2>
+        </div>
+        <div class="post">
+            <div class="image"></div>
+            <div class="text">
+                <h1>Sed et persipiatis unde</h1>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+            </div>
+        </div>
+    </div>
+    <div class="blogcontainer">
+        <div class="date">
+            <h1>30</h1>
+            <h2>JUL</h2>
+        </div>
+        <div class="post">
+            <div class="image"></div>
+            <div class="text">
+                <h1>Sed et persipiatis unde</h1>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="container">
+    <div class="blogcontainer">
+        <div class="date">
+            <h1>30</h1>
+            <h2>AUG</h2>
+        </div>
+        <div class="post">
+            <div class="image"></div>
+            <div class="text">
+                <h1>Sed et persipiatis unde</h1>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+            </div>
+        </div>
+    </div>
+    <div class="blogcontainer">
+        <div class="date">
+            <h1>23</h1>
+            <h2>NOV</h2>
+        </div>
+        <div class="post">
+            <div class="image"></div>
+            <div class="text">
+                <h1>Sed et persipiatis unde</h1>
+                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="blogcontainer">
+    <div class="date">
+        <h1>23</h1>
+        <h2>DEC</h2>
+    </div>
+    <div class="post">
+        <div class="image"></div>
+        <div class="text">
+            <h1>Sed et persipiatis unde</h1>
+            <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
+        </div>
+    </div>
+</div>
+</div>`;
+var contactContent = `<div class="contact">
+<div class="title">
+    <h1>CONTACT US:</h1>
+</div>
+<div class="contactcontainer">
+    <div class="form">
+        <input type="text" class="txtsub1" placeholder="Your Name...">
+        <input type="text" class="txtsub1" placeholder="Email Address...">
+        <input type="text" class="txtsub1" placeholder="Company...">
+        <input type="text" class="txtsub2" placeholder="Message...">
+    </div>
+    <div class="map"></div>
+</div>
+<div class="sendmessage">
+    <input type="submit" class="sendmessage" value="SEND MESSAGE">
+</div>
+</div>`;
+
+export function changePageContent(btnID){
+    let pageName = btnID + "Content";
+    $(app).html(eval(pageName));
+}
